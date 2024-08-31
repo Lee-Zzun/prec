4f5ac93e505396d0fbea59078d6c209dc44a1861
Lee-Zzun <ulysses8755@gmail.com>
2024-08-31 11:13:39 +0900

    추추가

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 # main.py
-
-import pandas as pd
+# streamlit을 이용한 주가 차트
 
 import streamlit as st
 from data_loader import load_data
 
 def main():
-    st.title('This is Dashboard')
+    st.title('This is Git Prec')
 
     df = load_data()
 
-    # 특정 날짜번위 선택
-    st.subheader('Select Date Range')
-    df['Date'] = pd.to_datetime(df['Date'])
-    start_date = st.date_input('Start date', df['Date'].min())
-    end_date = st.date_input('End date', df['Date'].max())
-    
-    ranged_df = df[(df['Date'] >= pd.to_datetime(start_date))
-                    & (df['Date'] >= pd.to_datetime(end_date))]
-    ranged_df = ranged_df.reset_index(drop=True)
-    st.table(ranged_df)
+    st.write(df)
 
 if __name__ == '__main__':
     main()
